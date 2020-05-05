3cd8eb28844d8df52dc20d43831981e9f4cad144
hector <hhuerta@siccob.com.mx>
2020-05-05 14:37:31 -0500

    Se agrega el encabezado

--- application/views/templates/Footer.php
+++ application/views/templates/Footer.php
@@ -1,26 +1,32 @@
 <footer>
     <div id="container">
         <div id="footer" class="row">
-            <div class="col-6 col-md-6 col-sm-6">
-                <img src="<?php echo base_url() ?>public/assets/img/cropped-orogold-logo-dgold.png" class="img-fluid float-left" alt="Responsive image">
-            </div>
-            <div class="col-3 col-md-3 col-sm-3">
-                <div class="row">
-                    <div class="col-2 col-md-2 col-sm-2">
-                        <img src="<?php echo base_url() ?>public/assets/icon/mail.png" class="img-fluid" alt="image">
+            <div id="marginFooter" class="row align-items-center">
+                <div id="marginLogo" class="col-6 col-md-6 col-sm-6">
+                    <div class="col-3 col-md-3 col-sm-3"></div>
+                    <div class="col-6 col-md-6 col-sm-6">
+                        <img id="divLogoFooter" src="<?php echo base_url() ?>public/assets/img/cropped-orogold-logo-dgold.png" class="img-fluid float-left" alt="Responsive image">
                     </div>
-                    <div class="col-10 col-md-10 col-sm-10">
-                        <p class="textoFooter card-text">orogoldspa@orogold.com</p>
+                    <div class="col-3 col-md-3 col-sm-3"></div>
+                </div>
+                <div class="col-3 col-md-3 col-sm-3">
+                    <div class="row">
+                        <div class="col-2 col-md-2 col-sm-2">
+                            <img src="<?php echo base_url() ?>public/assets/icon/mail.png" class="img-fluid" alt="image">
+                        </div>
+                        <div class="col-10 col-md-10 col-sm-10">
+                            <p class="textoFooter card-text">orogoldspa@orogold.com</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-3 col-md-3 col-sm-3">
-                <div class="row">
-                    <div class="col-2 col-md-2 col-sm-2">
-                        <img src="<?php echo base_url() ?>public/assets/icon/telephone.png" class="img-fluid" alt="image">
-                    </div>
-                    <div class="col-10 col-md-10 col-sm-10">
-                        <p class="textoFooter card-text">+ 52 5502630032</p>
+                <div class="col-3 col-md-3 col-sm-3">
+                    <div class="row">
+                        <div class="col-2 col-md-2 col-sm-2">
+                            <img src="<?php echo base_url() ?>public/assets/icon/telephone.png" class="img-fluid" alt="image">
+                        </div>
+                        <div class="col-10 col-md-10 col-sm-10">
+                            <p class="textoFooter card-text">+ 52 5502630032</p>
+                        </div>
                     </div>
                 </div>
             </div>
--- application/views/templates/Header.php
+++ application/views/templates/Header.php
@@ -4,7 +4,28 @@
         <meta charset="utf-8">
         <title>OROGOLD</title>
 
-        <link rel="stylesheet" href="<?php echo base_url() ?>public/assets/bootstrap/css/bootstrap.min.css">        
-        <style type="text/css">
-        </style>
+        <link rel="stylesheet" href="<?php echo base_url() ?>public/assets/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="<?php echo base_url() ?>public/assets/css/StyleHeader.css">
     </head>
+
+    <div id="container">
+        <div id="header" class="row">
+            <div class="row align-items-center">
+                <div class="col-6 col-md-6 col-sm-6">
+                    <div id="divLogoHeader" class="col-6 col-md-6 col-sm-6">
+                        <img src="<?php echo base_url() ?>public/assets/img/cropped-orogold-logo-dgold.png" class="img-fluid float-left" alt="Responsive image">
+                    </div>
+                </div>
+                <div class="col-3 col-md-3 col-sm-3">
+                    <div class="row">
+                        <div class="col-10 col-md-10 col-sm-10">
+                            <p class="textoHeader card-text">orogoldspa@orogold.com</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-3 col-md-3 col-sm-3">
+                    <button id="btnReserve" type="button" class="btn btn-outline-warning">Reserva ahora</button>
+                </div>
+            </div>
+        </div>
+    </div>
